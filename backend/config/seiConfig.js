d238804cd2551a5e549d43ddae65be9e6a630895
RishiS31 <rishi.s2022@vitstudent.ac.in>
2025-02-25 19:20:29 +0530

    Create seiConfig.js

---
+++ backend/config/seiConfig.js
@@ -0,0 +1,4 @@
+export const SEI_RPC_URL = "https://rpc.sei.network";
+export const SEI_CHAIN_ID = "pacific-1";
+export const STABLECOIN_CONTRACT = "sei1xyz..."; // Smart Contract Address
+export const PRIVATE_KEY = process.env.PRIVATE_KEY;
